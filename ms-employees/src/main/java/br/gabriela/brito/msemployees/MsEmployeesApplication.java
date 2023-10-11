37284e1d4aaa249c81810bac42e788ff3188d216
Gabriela Brito Vieira <gabriela.bvieira@hotmail.com>
2023-10-11 14:46:05 -0300

    feat: Implementing employee microservice

---
+++ ms-employees/src/main/java/br/gabriela/brito/msemployees/MsEmployeesApplication.java
@@ -0,0 +1,13 @@
+package br.gabriela.brito.msemployees;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MsEmployeesApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MsEmployeesApplication.class, args);
+	}
+
+}
