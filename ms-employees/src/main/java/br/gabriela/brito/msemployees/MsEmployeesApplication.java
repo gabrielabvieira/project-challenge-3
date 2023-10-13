e6245ee43226081e4c308f0d570b0a7cefd726c3
Gabriela Brito Vieira <gabriela.bvieira@hotmail.com>
2023-10-13 12:10:09 -0300

    feat: eureka annotation added in the application

--- ms-employees/src/main/java/br/gabriela/brito/msemployees/MsEmployeesApplication.java
+++ ms-employees/src/main/java/br/gabriela/brito/msemployees/MsEmployeesApplication.java
@@ -2,8 +2,11 @@ package br.gabriela.brito.msemployees;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
 
 @SpringBootApplication
+@EnableEurekaClient
+
 public class MsEmployeesApplication {
 
 	public static void main(String[] args) {
