104230e6ce30650c7b5989e70f741706ef57b8d0
Gabriela Brito Vieira <gabriela.bvieira@hotmail.com>
2023-10-15 20:15:08 -0300

    feat: employees mapping, class that receives requests from the controller and employees service to serve the controller added

--- ms-employees/src/main/java/br/gabriela/brito/msemployees/application/EmployeesController.java
+++ ms-employees/src/main/java/br/gabriela/brito/msemployees/application/EmployeesController.java
@@ -1,2 +1,43 @@
-package br.gabriela.brito.msemployees.application;public class EmployeesController {
+package br.gabriela.brito.msemployees.application;
+
+import br.gabriela.brito.msemployees.application.representation.EmployeesSaveRequest;
+import br.gabriela.brito.msemployees.domain.Employees;
+import lombok.RequiredArgsConstructor;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+import org.springframework.web.servlet.support.ServletUriComponentsBuilder;
+
+import java.net.URI;
+
+@RestController
+@RequestMapping("api/v1/employees")
+@RequiredArgsConstructor
+public class EmployeesController {
+
+    private final EmployeesService service;
+    @GetMapping
+    public String status(){
+        return "ok";
+    }
+
+    @PostMapping
+    public ResponseEntity save(@RequestBody EmployeesSaveRequest request){
+        var employees = request.toModel();
+        service.save(employees);
+        URI headerLocation = ServletUriComponentsBuilder
+                .fromCurrentRequest()
+                .query("cpf={cpf}")
+                .buildAndExpand(employees.getCpf())
+                .toUri();
+        return ResponseEntity.created(headerLocation).build();
+    }
+
+    @GetMapping(params = "cpf")
+    public ResponseEntity dataEmployees(@RequestParam("cpf") String cpf){
+        var employees = service.getByCPF(cpf);
+        if(employees.isEmpty()){
+            return ResponseEntity.notFound().build();
+        }
+        return ResponseEntity.ok(employees);
+    }
 }
--- ms-employees/src/main/java/br/gabriela/brito/msemployees/application/EmployeesService.java
+++ ms-employees/src/main/java/br/gabriela/brito/msemployees/application/EmployeesService.java
@@ -1,2 +1,25 @@
-package br.gabriela.brito.msemployees.application;public class EmployeesService {
+package br.gabriela.brito.msemployees.application;
+
+import br.gabriela.brito.msemployees.domain.Employees;
+import br.gabriela.brito.msemployees.infra.repository.EmployeesRepository;
+import jakarta.transaction.Transactional;
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Service;
+
+import java.util.Optional;
+
+@Service
+@RequiredArgsConstructor
+public class EmployeesService {
+
+    private final EmployeesRepository repository;
+
+    @Transactional
+    public Employees save(Employees employees){
+        return repository.save(employees);
+    }
+
+    public Optional<Employees> getByCPF(String cpf){
+        return repository.findByCpf(cpf);
+    }
 }
