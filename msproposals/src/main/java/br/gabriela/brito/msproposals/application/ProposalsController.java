40e9c0e4125ed4b298e927b32947f6fbd39b76ad
Gabriela Brito Vieira <gabriela.bvieira@hotmail.com>
2023-10-16 14:39:26 -0300

    creating controller and testing api return

--- msproposals/src/main/java/br/gabriela/brito/msproposals/application/ProposalsController.java
+++ msproposals/src/main/java/br/gabriela/brito/msproposals/application/ProposalsController.java
@@ -1,2 +1,15 @@
-package br.gabriela.brito.msproposals.application;public class ProposalsController {
+package br.gabriela.brito.msproposals.application;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping ("api/v1/proposals")
+public class ProposalsController {
+
+    @GetMapping
+    public String status(){
+        return "ok";
+    }
 }
