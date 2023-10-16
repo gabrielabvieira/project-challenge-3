17149533afb582a3614cf2a6868e060bc6fd54e6
Gabriela Brito Vieira <gabriela.bvieira@hotmail.com>
2023-10-16 14:21:56 -0300

    feat: implementing proposal microservice

---
+++ msproposals/src/main/java/br/gabriela/brito/msproposals/MsproposalsApplication.java
@@ -0,0 +1,13 @@
+package br.gabriela.brito.msproposals;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MsproposalsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MsproposalsApplication.class, args);
+	}
+
+}
---
+++ msproposals/src/main/java/br/gabriela/brito/msproposals/application/ProposalsController.java
@@ -0,0 +1,2 @@
+package br.gabriela.brito.msproposals.application;public class ProposalsController {
+}
