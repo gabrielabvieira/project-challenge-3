b797c124695d980da34d0e0827e781cb90fc2735
Gabriela Brito Vieira <gabriela.bvieira@hotmail.com>
2023-10-17 14:22:03 -0300

    feat: synchronous communication with OpenFeign

--- msvoting/src/main/java/br/gabriela/brito/msvoting/application/VotingSessionController.java
+++ msvoting/src/main/java/br/gabriela/brito/msvoting/application/VotingSessionController.java
@@ -1,11 +1,12 @@
 package br.gabriela.brito.msvoting.application;
 
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
-@RequestMapping("voting-session")
+@RequestMapping("api/v1/voting-session")
 public class VotingSessionController {
 
     @GetMapping
