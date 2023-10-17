986b0fce9834079021d15004e534d9ef831ffb0b
Gabriela Brito Vieira <gabriela.bvieira@hotmail.com>
2023-10-17 00:04:38 -0300

    feat: implementing voting microservice

---
+++ msvoting/src/main/java/br/gabriela/brito/msvoting/MsvotingApplication.java
@@ -0,0 +1,13 @@
+package br.gabriela.brito.msvoting;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MsvotingApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MsvotingApplication.class, args);
+	}
+
+}
