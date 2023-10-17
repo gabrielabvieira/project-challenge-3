d6f4e6a0f1914d2d61239b9a256a4f6f9306cce7
Gabriela Brito Vieira <gabriela.bvieira@hotmail.com>
2023-10-17 13:58:52 -0300

    feat: structure of input data when creating a voting session

--- msvoting/src/main/java/br/gabriela/brito/msvoting/application/representation/VotingSessionSaveRequest.java
+++ msvoting/src/main/java/br/gabriela/brito/msvoting/application/representation/VotingSessionSaveRequest.java
@@ -1,2 +1,11 @@
-package br.gabriela.brito.msvoting.application.representation;public class VotingSessionSaveRequest {
+package br.gabriela.brito.msvoting.application.representation;
+
+import lombok.Data;
+
+@Data
+public class VotingSessionSaveRequest {
+    private String proposalId;
+    private int sessionTime;
+    private String employeeCpf;
 }
+
