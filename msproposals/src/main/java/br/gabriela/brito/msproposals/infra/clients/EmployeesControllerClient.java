d4ae563e11ed693d850f5c57e9e2d6ec909f32c7
Gabriela Brito Vieira <gabriela.bvieira@hotmail.com>
2023-10-18 01:01:05 -0300

    feat: connecting employee microservices with proposals for CPF validation

--- msproposals/src/main/java/br/gabriela/brito/msproposals/infra/clients/EmployeesControllerClient.java
+++ msproposals/src/main/java/br/gabriela/brito/msproposals/infra/clients/EmployeesControllerClient.java
@@ -1,2 +1,17 @@
-package br.gabriela.brito.msproposals.infra.clients;public interface EmployeesControllerClient {
+package br.gabriela.brito.msproposals.infra.clients;
+
+import br.gabriela.brito.msproposals.domain.Proposal;
+import org.springframework.cloud.openfeign.FeignClient;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+
+import java.util.List;
+
+@FeignClient(value = "msemployees", path = "/api/v1/employees")
+public interface EmployeesControllerClient {
+
+    @GetMapping
+    void validateEmployee(@RequestParam("cpf") String cpf);
+    @GetMapping
+    List<Proposal> getProposalsByEmployee(@RequestParam("cpf") String cpf);
 }
